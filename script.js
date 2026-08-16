0528118a1918e961430cfdd621efe9e24e6db716
Aryan Singhai <162977864+Aryan-Singhai@users.noreply.github.com>
2026-08-17 00:34:06 +0530

    Add mobile navigation functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
